64e7c1e015cdcc658b20eb9b2b157ab938618143
gusals00 <gusals6586@gmail.com>
2022-02-15 15:20:38 +0900

    init

---
+++ src/test/java/jpa/enrolment/EnrolmentApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa.enrolment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrolmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
