94351c53b294989277b182b15d00b92d41273b1e
HO <love47024702@naver.com>
2022-02-16 17:17:58 +0900

    엔티티구현

--- src/main/java/jpa/enrolment/domain/person/Admin.java
+++ src/main/java/jpa/enrolment/domain/person/Admin.java
@@ -1,12 +1,24 @@
 package jpa.enrolment.domain.person;
 
+import jpa.enrolment.domain.Department;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Admin extends Person {
 
+    protected Admin(String ssn, String name, String email, String loginId, String loginPw, Department department) {
+        super(ssn, name, email, loginId, loginPw, department);
+    }
+
+    public static Admin createAdmin(){
+        return new Admin();
+    }
 }
 
--- src/main/java/jpa/enrolment/domain/person/Professor.java
+++ src/main/java/jpa/enrolment/domain/person/Professor.java
@@ -1,13 +1,27 @@
 package jpa.enrolment.domain.person;
 
+import jpa.enrolment.domain.Department;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Professor extends Person {
 
     private String phoneNumber;
     private String labNumber;
+
+    protected Professor(String ssn, String name, String email, String loginId, String loginPw, Department department, String phoneNumber, String labNumber) {
+        super(ssn, name, email, loginId, loginPw, department);
+        this.phoneNumber = phoneNumber;
+        this.labNumber = labNumber;
+    }
+
+    public static Professor createProfessor(String ssn, String name, String email, String loginId, String loginPw, Department department, String phoneNumber, String labNumber){
+        return new Professor(ssn,  name,  email,  loginId,  loginPw,  department,  phoneNumber,  labNumber);
+    }
 }
