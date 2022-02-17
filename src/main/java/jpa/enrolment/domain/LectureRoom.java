ef64cae2b428cebf2addaf4a4b5a1d9d83b7ff3d
HO <love47024702@naver.com>
2022-02-17 14:28:42 +0900

    BuildingName 수정

--- src/main/java/jpa/enrolment/domain/LectureRoom.java
+++ src/main/java/jpa/enrolment/domain/LectureRoom.java
@@ -1,11 +1,15 @@
 package jpa.enrolment.domain;
 
+import jpa.enrolment.domain.building.BuildingName;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LectureRoom {
     @Id @GeneratedValue
     @Column(name = "lecture_room_id")
@@ -15,4 +19,13 @@ public class LectureRoom {
     private BuildingName buildingName;
 
     private int roomNumber;
+
+    protected LectureRoom(BuildingName buildingName, int roomNumber) {
+        this.buildingName = buildingName;
+        this.roomNumber = roomNumber;
+    }
+
+    public static LectureRoom createLectureRoom(BuildingName buildingName, int roomNumber){
+        return new LectureRoom(buildingName,roomNumber);
+    }
 }
---
+++ src/main/java/jpa/enrolment/domain/building/BuildingName.java
@@ -0,0 +1,6 @@
+package jpa.enrolment.domain.building;
+
+
+public enum BuildingName {
+    D,G,T // DIGITAL, GLOBAL, TECHNO
+}
